20147391d8feade34b7b2e974af596094b0da79f
SeWonKim <30452963+Siromom@users.noreply.github.com>
2020-04-28 22:53:26 +0900

    2주차 복습

--- 2주차/2-1.cpp
+++ 2주차/2-1.cpp
@@ -5,3 +5,38 @@
     - reverse 메소드 수행 후 return vector에 넣어준다.
 3. 2를 n번 반복
 */
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<string> solution(int n, vector<int> arr1, vector<int> arr2)
+{
+    vector<string> answer;
+
+    for (int i = 0; i < n; i++)
+    {
+        string str = "";
+        int tmp = arr1[i] | arr2[i];
+
+        for (int j = 0; j < n; j++)
+        {
+            if (tmp % 2 == 0)
+            {
+                str += ' ';
+            }
+            else
+            {
+                str += '#';
+            }
+            tmp = tmp >> 1;
+        }
+
+        reverse(str.begin(), str.end());
+        answer.push_back(str);
+    }
+    return answer;
+}
--- 2주차/2-3.cpp
+++ 2주차/2-3.cpp
@@ -9,6 +9,7 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <map>
 
 using namespace std;
 
@@ -53,5 +54,61 @@ int main()
             last = str[i];
         }
     }
+
+    return 0;
+}
+
+/*
+
+중복 처리에 map 사용한 코드
+
+*/
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+using namespace std;
+
+bool cmp(const pair<string, int> &a, const pair<string, int> &b)
+{
+    if (a.second == b.second)
+    {
+        return (a.first < b.first);
+    }
+    return (a.second < b.second);
+}
+
+int main()
+{
+    int n;
+    vector<pair<string, int>> words; // 단어, 단어의 길이
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        string str;
+        cin >> str;
+        words.push_back(make_pair(str, str.size()));
+    }
+
+    sort(words.begin(), words.end(), cmp);
+
+    map<string, int> m;
+    for (int i = 0; i < n; i++)
+    {
+        if (m[words[i].first] > 0)
+        {
+            continue;
+        }
+        else
+        {
+            m[words[i].first]++;
+        }
+        cout << words[i].first << endl;
+    }
+
     return 0;
 }
